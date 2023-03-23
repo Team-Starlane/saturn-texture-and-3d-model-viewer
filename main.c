56fdb889d4a47f3215c7f5c9e2643dc0a10f117f
ReyeMe <gphys25@gmail.com>
2023-03-23 22:31:16 +0100

    Code upload

--- main.c
+++ main.c
@@ -3,38 +3,16 @@
 // Take it from this subfolder.
 // IMPORTANT: Do not write file extensions in capital letters in Linux! Case sensitive OS.
 #include "ZT/ZT_COMMON.h"
-#include "island.h"
 
-/*  Image variables.
-    
-    XN: I believe that in Jo the maximum number of 8 bit palettes is 4 (4 x 256 = 1024).
-    Palette colors are not shared across multiple palettes.
-    
-    Consider using a single palette for multiple images and coordinating the indices of colors
-    across multiple images.
+// Current model and total number of models
+static int CurrentEntity = 0;
+static int TotalEntities = 0;
 
-    E.g. Image 1 takes color indices 0-7 and images 2 takes indices 8-15 and so forth.
-
-*/
-static jo_palette       palette1;
-static jo_palette       palette2;
-static jo_palette       palette3;
-static jo_palette       palette4;
-static jo_palette       palette5;
-static jo_palette       palette6;
-static jo_palette       palette7;
-static jo_palette       palette8;
-
-/*  XN:
-
-    A.tga = Display mesh.
-    B through E.tga = LOD levels 0 to 3.
-
-*/ 
-#define IMG_MAX 6
-enum images{a_dot_tga, b_dot_tga, c_dot_tga, d_dot_tga, e_dot_tga, f_dot_tga};
-int img_loaded = a_dot_tga;
-int spriteIndex = 0;
+// Viewport angle
+#define FOV_MIN (DEGtoANG(10))
+#define FOV_MAX (DEGtoANG(160))
+#define FOV_STEP (DEGtoANG(1))
+static ANGLE Fov = (DEGtoANG(70));
 
 // Values for rotation. Base matrix rotation value and rotation frustrum.
 int rot_multiplier = 1;
@@ -45,7 +23,6 @@ int rot_neg_frust = -90;
 int rot_pos_frust = 90;
 int neg_frust_ext = -180;
 int pos_frust_ext = 180;
-bool toggle_auto_rot;
 
 // Values for z-depth.
 #define Z_BASE 75
@@ -54,58 +31,31 @@ int far = 1000;
 int near = 200;
 int frustrum = 20;
 
-/* Main island */
-static BlueSkiesIsland Island;
-
-// Reyes stuff.
-/*
-void            my_draw(void)
-{
-    jo_sprite_set_palette(spriteIndex);
-    jo_sprite_draw3D(spriteIndex, 0, 0, 500);
-}
-*/
-
 void			my_input(void)
 {
-    // IMG_MAX is the number of images you put into your cd root.
-    // Cycle through all compiled images.
-    if  (jo_is_pad1_key_down(JO_KEY_A) && spriteIndex < IMG_MAX)
+    // Cycle through all models
+    if  (jo_is_pad1_key_down(JO_KEY_A) && CurrentEntity < TotalEntities)
     {
-        spriteIndex++;
+        CurrentEntity++;
     }
-    else if (jo_is_pad1_key_down(JO_KEY_A) && spriteIndex > IMG_MAX)
+    else if (jo_is_pad1_key_down(JO_KEY_B) && CurrentEntity >= 0)
     {
-        spriteIndex == 0;
+        CurrentEntity--;
     }
-    else if (jo_is_pad1_key_down(JO_KEY_B) && spriteIndex >= 0)
+
+    // Handle wrapping around
+    if (CurrentEntity >= TotalEntities)
     {
-        spriteIndex--;
+        CurrentEntity = 0;
     }
-    else if (jo_is_pad1_key_down(JO_KEY_B) && spriteIndex < 0)
+    else if (CurrentEntity < 0)
     {
-        spriteIndex == IMG_MAX;
+        CurrentEntity = TotalEntities - 1;
     }
 
     if  (jo_is_pad1_key_pressed(JO_KEY_C))
     {
-        
         z+=(z>>2) / 25 + 1;
-        
-        /*
-		if(z > far)
-		{
-			z = z+12;
-		}
-		else if (z > near)
-        {
-            z = z+3;
-        }
-        else
-		{
-			z++;
-		}
-        */
     }
     else if (jo_is_pad1_key_pressed(JO_KEY_Z))
     {
@@ -114,25 +64,6 @@ void			my_input(void)
         }
         // Readding angry smiley:
         else{z++;} 
-
-        /*
-		if(z > far)
-		{
-			z = z-12;
-		}
-		else if (z > near)
-        {
-            z = z-3;
-        }
-        else if (z > frustrum)
-		{
-			z--;
-		}
-        else
-        {
-            z++;
-        }
-        */
     }
     
     // Horizontal axis.
@@ -191,169 +122,74 @@ void			my_input(void)
         rot_z-=rot_multiplier;
     }
 
+    if (jo_is_pad1_key_pressed(JO_KEY_X) && Fov > FOV_MIN)
+    {
+        Fov -= FOV_STEP;
+        slPerspective(Fov);
+    }
+    else if (jo_is_pad1_key_pressed(JO_KEY_Y) && Fov < FOV_MAX)
+    {
+        Fov += FOV_STEP;
+        slPerspective(Fov);
+    }
+
     // Reset all values.
     if (jo_is_pad1_key_pressed(JO_KEY_START))
     {
-    rot_x = 0;
-    rot_y = 0;
-    rot_z = 0;
-    z = Z_BASE;
+        rot_x = 0;
+        rot_y = 0;
+        rot_z = 0;
+        z = Z_BASE;
     }
 }
 
 void        my_draw(void)
 {
-    // New ZTP draw code.
-    MATRIX matrix;
-    
     jo_3d_push_matrix();
     {
-        // Set 3D matrix
-        
-        // Don't wanna use Reyes coordinate system.
-        // ExGetMatrixToCoordinateSystem(&Camera, matrix);
-        // slLoadMatrix(matrix);
-
         jo_3d_translate_matrix_z(z);
         jo_3d_rotate_matrix_x(rot_x);
         jo_3d_rotate_matrix_y(rot_y);
         jo_3d_rotate_matrix_z(rot_z);
 
-        // Draw island
-        IslandDraw(&Island);
-        
-        // Draw water
-        //jo_background_3d_plane_a_draw(true);
-        //jo_background_3d_plane_b_draw(true);
+        if (CurrentEntity < TotalEntities && CurrentEntity >= 0)
+        {
+            for(int mesh = 0; mesh < entities[CurrentEntity].nbMeshes; mesh++)
+	        {
+	            slPutPolygon((XPDATA*)entities[CurrentEntity].pol[mesh]);
+	        }
+        }
     }
     jo_3d_pop_matrix();
     
-    /* Old draw code
-
-    // Setting palette every frame seems not to matter.
-    // Push matrix, apply transformations from button inputs, draw sprite, pop matrix.
-    jo_sprite_set_palette(palette1.id);
-    jo_3d_push_matrix();
-    jo_3d_translate_matrix_z(z);
-    jo_3d_rotate_matrix_x(rot_x);
-    jo_3d_rotate_matrix_y(rot_y);
-    jo_3d_rotate_matrix_z(rot_z);
-    jo_3d_create_sprite_quad (spriteIndex);
-    jo_3d_pop_matrix();
-
-    */
-
     // Clear screen line 2 and following, because the numbers freak otherwise, when z value is updated.
     // Don't clear line 0, because clearing is broke on line 0.
-    jo_clear_screen_line(2);
-    jo_clear_screen_line(3);
-    jo_clear_screen_line(4);
-    jo_clear_screen_line(5);
-    jo_clear_screen_line(6);
-    jo_printf(0, 0, "Press Start to reset all values.");
-    jo_printf(0, 2, "Z-depth (press C or Z): %d", z);
-    jo_printf(0, 3, "Cycle images with A & B: Img. Nr.: %d", spriteIndex);
-    jo_printf(0, 4, "Rotat. on x (press Up or Down): %d", rot_x);
-    jo_printf(0, 5, "Rotat. on y (press Left or Right): %d", rot_y);
-    jo_printf(0, 6, "Rotat. on z (press L or R trigger): %d", rot_z);
-}
-
-jo_palette      *my_tga_palette_handling(void)
-{
-    if(img_loaded == a_dot_tga)
-        {
-            jo_create_palette(&palette1);
-            return (&palette1);
-        }
-    else if(img_loaded == b_dot_tga)
-        {
-            jo_create_palette(&palette2);
-            return (&palette2);
-        }
-    else if(img_loaded == c_dot_tga)
-        {
-            jo_create_palette(&palette3);
-            return (&palette3);
-        }
-    else if(img_loaded == d_dot_tga)
-        {
-            jo_create_palette(&palette4);
-            return (&palette4);
-        }
-    else if(img_loaded == e_dot_tga)
-        {
-            jo_create_palette(&palette5);
-            return (&palette5);
-        }
-    else if(img_loaded == f_dot_tga)
-        {
-            jo_create_palette(&palette6);
-            return (&palette6);
-        }
+    jo_printf(0, 0, "Press Start to reset all values.         ");
+    jo_printf(0, 2, "Z-depth (press C or Z): %d               ", z);
+    jo_printf(0, 3, "Model: %d/%d                             ", CurrentEntity + 1, TotalEntities);
+    jo_printf(0, 4, "Rotat. on x (press Up or Down): %d       ", rot_x);
+    jo_printf(0, 5, "Rotat. on y (press Left or Right): %d    ", rot_y);
+    jo_printf(0, 6, "Rotat. on z (press L or R trigger): %d   ", rot_z);
+    jo_printf(0, 7, "FOV: %d                                  ", jo_fixed2int(slAng2FX(Fov)));
 }
 
 /** @brief Load assets
  */
 void LoadAssets()
 {
-    jo_core_tv_off();
-
-    /* Not using RGB planes.
-    // set palette loading
-    jo_set_tga_palette_handling(Rgb0PaletteHandling);
-
-    // Enable RGB0
-    jo_enable_background_3d_plane(JO_COLOR_RGB(71,243,255));
-    
-    // Load water layer texture
-    jo_img_8bits img;
-    img.data = JO_NULL;
-    jo_tga_8bits_loader(&img, JO_ROOT_DIR, "WATER.TGA", 0);
-    jo_background_3d_plane_a_img(&img, Rgb0Palette.id, true, true);
-    jo_background_3d_plane_b_img(&img, Rgb0Palette.id, true, true);
-    jo_free_img(&img);
-    */
+    // Set default perspective
+    slPerspective(Fov);
 
 	//	Start loading in LWRAM beginning
     void * currentAddress = (void*)LWRAM;
     int entityIds = 0;
 
-    // Load island model
-    IslandInitialize(currentAddress, &entityIds, &Island);
+    // Load models
+    // To add new ones just duplicate this line
+    currentAddress = ztLoad3Dmodel((Sint8*)"IS00DS_G.ZTP", currentAddress, entityIds++, false);
 
-    jo_core_tv_on();
-}
-
-void            add_image_files(void)
-{
-    jo_set_tga_palette_handling(my_tga_palette_handling);
-    jo_sprite_add_tga(JO_ROOT_DIR, "A.TGA", 1);
-    jo_set_tga_palette_handling(JO_NULL);
-
-    img_loaded = b_dot_tga;
-    jo_set_tga_palette_handling(my_tga_palette_handling);
-    jo_sprite_add_tga(JO_ROOT_DIR, "B.TGA", 1);
-    jo_set_tga_palette_handling(JO_NULL);
-
-    img_loaded = c_dot_tga;
-    jo_set_tga_palette_handling(my_tga_palette_handling);
-    jo_sprite_add_tga(JO_ROOT_DIR, "C.TGA", 1);
-    jo_set_tga_palette_handling(JO_NULL);
-
-    img_loaded = d_dot_tga;
-    jo_set_tga_palette_handling(my_tga_palette_handling);
-    jo_sprite_add_tga(JO_ROOT_DIR, "D.TGA", 1);
-    jo_set_tga_palette_handling(JO_NULL);
-
-    img_loaded = e_dot_tga;
-    jo_set_tga_palette_handling(my_tga_palette_handling);
-    jo_sprite_add_tga(JO_ROOT_DIR, "E.TGA", 1);
-    jo_set_tga_palette_handling(JO_NULL);
-
-    img_loaded = f_dot_tga;
-    jo_set_tga_palette_handling(my_tga_palette_handling);
-    jo_sprite_add_tga(JO_ROOT_DIR, "F.TGA", 1);
-    jo_set_tga_palette_handling(JO_NULL);
+    // Set our total model count
+    TotalEntities = entityIds;
 }
 
 void			jo_main(void)
@@ -361,7 +197,7 @@ void			jo_main(void)
 	jo_core_init(JO_COLOR_Black);
 
     LoadAssets();
-    // add_image_files();
+    
     jo_core_add_callback(my_input);
     jo_core_add_callback(my_draw);
 
